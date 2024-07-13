6d33b05138aa73107777fa9c9be6762f28ec47fd
Ishwar Chaudhary <ishwarura@gmail.com>
2024-07-13 22:01:37 +0545

    Prelogin UI updated

--- lib/features/prelogin/presentation/widgets/services_card_widget.dart
+++ lib/features/prelogin/presentation/widgets/services_card_widget.dart
@@ -14,42 +14,54 @@ class ServicesCard extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Expanded(
-            child: SizedBox(
-              height: 110,
-              child: CardWidgetPre(
-                title: AppLocalizations.of(context).translate('waste_pickup'),
-                routeName: "/hospital",
-                imagePath: AppImages.cardImage1,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: SizedBox(
+                height: 110,
+                child: CardWidgetPre(
+                  title: AppLocalizations.of(context).translate('waste_pickup'),
+                  routeName: "/hospital",
+                  imagePath: AppImages.cardImage1,
+                ),
               ),
             ),
           ),
           Expanded(
-            child: SizedBox(
-              height: 110,
-              child: CardWidgetPre(
-                title: AppLocalizations.of(context).translate('sell'),
-                routeName: "/appointment",
-                imagePath: AppImages.cardImage2,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: SizedBox(
+                height: 110,
+                child: CardWidgetPre(
+                  title: AppLocalizations.of(context).translate('sell'),
+                  routeName: "/appointment",
+                  imagePath: AppImages.cardImage2,
+                ),
               ),
             ),
           ),
           Expanded(
-            child: SizedBox(
-              height: 110,
-              child: CardWidgetPre(
-                title: AppLocalizations.of(context).translate('donate'),
-                routeName: "/appointment",
-                imagePath: AppImages.cardImage3,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: SizedBox(
+                height: 110,
+                child: CardWidgetPre(
+                  title: AppLocalizations.of(context).translate('donate'),
+                  routeName: "/appointment",
+                  imagePath: AppImages.cardImage3,
+                ),
               ),
             ),
           ),
           Expanded(
-            child: SizedBox(
-              height: 110,
-              child: CardWidgetPre(
-                title: AppLocalizations.of(context).translate('blogs'),
-                routeName: "/appointment",
-                imagePath: AppImages.cardImage4,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: SizedBox(
+                height: 110,
+                child: CardWidgetPre(
+                  title: AppLocalizations.of(context).translate('blogs'),
+                  routeName: "/appointment",
+                  imagePath: AppImages.cardImage4,
+                ),
               ),
             ),
           ),
